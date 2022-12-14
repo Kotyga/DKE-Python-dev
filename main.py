e9a746b7dc6efd2a2a87a40f70f7502b6c2418e2
Alex <42418433+aiexz@users.noreply.github.com>
2022-12-14 17:33:30 +0300

    Add all db from access

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import fastapi
 from fastapi.templating import Jinja2Templates
+import pyodbc
+connstr = (r'DRIVER={Microsoft Access Driver (.mdb,.accdb)};'r'DBQ=C:\Users\aobus\Downloads\online-productstore.accdb;')
+cnxn = pyodbc.connect(connstr)
 
 app = fastapi.FastAPI()
 templates = Jinja2Templates(directory="./templates")
 
 @app.get("/")
 def read_root(request: fastapi.Request):
-    return templates.TemplateResponse("all_db.html", {"request": request, "databases": ["hello", "world"] })
+    crsr = cnxn.cursor()
+    tables = [ x for x in crsr.tables(tableType='TABLE')]
+    return templates.TemplateResponse("all_db.html", {"request": request, "databases": tables })
